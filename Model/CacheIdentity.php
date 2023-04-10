6d5667605d7e99fbde3713077cedfe75896adc05
Damien Retzinger <damienwebdev@gmail.com>
2023-04-10 09:52:29 -0400

    feat(eav, graphql): cache customAttributeMetadata (#2)

--- Model/CacheIdentity.php
+++ Model/CacheIdentity.php
@@ -1,13 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See https://github.com/mage-os/mageos-magento2/blob/2.4-develop/COPYING.txt for license details.
+ */
+
 namespace Graycore\CustomAttributeMetadataCache\Model;
 
+use Magento\Eav\Model\Entity\Attribute as EavAttribute;
 use Magento\Framework\GraphQl\Query\Resolver\IdentityInterface;
 
 class CacheIdentity implements IdentityInterface
 {
+    /**
+     * @inheritDoc
+     */
     public function getIdentities(array $resolvedData): array
     {
-        return [];
+        $identities = [];
+        if (isset($resolvedData['items']) && !empty($resolvedData['items'])) {
+            foreach ($resolvedData['items'] as $item) {
+                if (is_array($item)) {
+                    $identities[] = sprintf(
+                        "%s_%s_%s",
+                        EavAttribute::CACHE_TAG,
+                        $item['entity_type'],
+                        $item['attribute_code']
+                    );
+                }
+            }
+        } else {
+            return [];
+        }
+        return $identities;
     }
 }
